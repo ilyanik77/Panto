924291dce239b169a52b356b04573f55aab5a15d
ilya <ilyakhlamov77@gmail.com>
2024-02-17 21:02:53 +0300

    add 17.02.2024

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -8,6 +8,21 @@ const swiper = new Swiper('.swiper', {
 	freeMode: true,
 	// speed: 1000,
 
+	// breakpoints: {
+	// 	640: {
+	// 		slidesPerView: 2,
+	// 		spaceBetween: 20,
+	// 	},
+	// 	768: {
+	// 		slidesPerView: 4,
+	// 		spaceBetween: 40,
+	// 	},
+	// 	1024: {
+	// 		slidesPerView: 5,
+	// 		spaceBetween: 50,
+	// 	},
+	// },
+
 	navigation: {
 		nextEl: '#sliderNext',
 		prevEl: '#sliderPrev',
